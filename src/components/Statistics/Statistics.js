1bc3d3cdf8ff5fcb08c39c253a1049b6bcfe27b0
Vlad-Kozak <k911k12@gmail.com>
2022-06-28 09:38:21 +0300

    fix propTypes

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -18,6 +18,13 @@ function Statistics({ title, stats }) {
   );
 }
 
-Statistics.propTypes = {};
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
 
 export default Statistics;
